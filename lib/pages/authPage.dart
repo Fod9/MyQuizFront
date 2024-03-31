ab0c4f1eee9d70526fd14d3b4b1df94e6d8b8f6e
Sandra <sandra.sini@ecoles-epsi.net>
2024-03-31 17:11:02 +0200

    validators formulaires

--- lib/pages/authPage.dart
+++ lib/pages/authPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../helpers/Colors.dart';
-import 'package:my_quiz_ap/components/sizedBlock.dart';
+import '../helpers/validators.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key, required this.screenType});
@@ -17,6 +17,62 @@ class _AuthPageState extends State<AuthPage> {
   String authType = "inscription";
   bool isLoginFormDisplayed = false;
 
+  late TextEditingController nameController = TextEditingController();
+  late TextEditingController firstnameController = TextEditingController();
+  late TextEditingController emailController = TextEditingController();
+  late TextEditingController usernameController = TextEditingController();
+  late TextEditingController pwdController = TextEditingController();
+  late TextEditingController confirmPwdController = TextEditingController();
+
+  String? nameError;
+  String? emailError;
+  String? usernameError;
+  String? pwdError;
+  String? confirmPwdError;
+
+  final formKeyInscription = GlobalKey<FormState>();
+  final formKeyConnexion = GlobalKey<FormState>();
+
+  void handleTextFieldChange(String type, String value) {
+    switch (type) {
+      case "nom":
+        setState(() => nameError = Validator.validateName(value));
+        break;
+      case "prenom":
+        setState(() => nameError = Validator.validateName(value));
+        break;
+      case "email":
+        setState(() => emailError = Validator.validateEmail(value));
+        break;
+      case "pseudo":
+        setState(() => usernameError = Validator.validateUsername(value));
+        break;
+      case "mdp":
+        setState(() => pwdError = Validator.validatePassword(value));
+        setState(() {
+          if (confirmPwdController.text.isNotEmpty) {
+            confirmPwdError = Validator.comparePasswords(confirmPwdController.text, value);
+          }
+        });
+        break;
+      case "confirmMdp":
+        setState(() => confirmPwdError = Validator.comparePasswords(value, pwdController.text));
+        break;
+    }
+  }
+
+  void handleFormSubmit(String formType) {
+    if (formType == "inscription") {
+      if (formKeyInscription.currentState!.validate()) {
+        // Form submission logic for inscription form
+      }
+    } else {
+      if (formKeyConnexion.currentState!.validate()) {
+        // Form submission logic for connexion form
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     if (widget.screenType == "mobile") {
@@ -25,8 +81,27 @@ class _AuthPageState extends State<AuthPage> {
     }
 
     return (widget.screenType == "mobile" || _width > 600)
-        ? MobileDisplay(width: _width, height: _height, toggleForm: toggleForm, isLoginFormDisplayed: isLoginFormDisplayed)
-        : DesktopDisplay(width: _width, height: _height);
+      ? MobileDisplay(
+        width: _width,
+        height: _height,
+        toggleForm: toggleForm,
+        isLoginFormDisplayed: isLoginFormDisplayed,
+        formKeyInscription: formKeyInscription,
+        formKeyConnexion: formKeyConnexion,
+        nameController: nameController,
+        firstnameController: firstnameController,
+        emailController: emailController,
+        usernameController: usernameController,
+        pwdController: pwdController,
+        confirmPwdController: confirmPwdController,
+        nameError: nameError,
+        emailError: emailError,
+        usernameError: usernameError,
+        pwdError: pwdError,
+        confirmPwdError: confirmPwdError,
+        handleTextFieldChange: handleTextFieldChange,
+      )
+      : DesktopDisplay(width: _width, height: _height);
   }
   void toggleForm() {
     setState(() {
@@ -37,15 +112,48 @@ class _AuthPageState extends State<AuthPage> {
 
 //create a widget
 class MobileDisplay extends StatelessWidget {
-  MobileDisplay({Key? key, required this.width, required this.height, required this.toggleForm, required this.isLoginFormDisplayed}) : super(key: key);
+  const MobileDisplay({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.toggleForm,
+    required this.isLoginFormDisplayed,
+    required this.formKeyInscription,
+    required this.formKeyConnexion,
+    required this.nameController,
+    required this.firstnameController,
+    required this.emailController,
+    required this.usernameController,
+    required this.pwdController,
+    required this.confirmPwdController,
+    required this.nameError,
+    required this.emailError,
+    required this.usernameError,
+    required this.pwdError,
+    required this.confirmPwdError,
+    required this.handleTextFieldChange,
+  }) : super(key: key);
 
   final double width;
   final double height;
   final void Function() toggleForm;
-  final formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> formKeyInscription;
+  final GlobalKey<FormState> formKeyConnexion;
   final bool isLoginFormDisplayed;
+  final TextEditingController nameController;
+  final TextEditingController firstnameController;
+  final TextEditingController emailController;
+  final TextEditingController usernameController;
+  final TextEditingController pwdController;
+  final TextEditingController confirmPwdController;
+  final String? nameError;
+  final String? emailError;
+  final String? usernameError;
+  final String? pwdError;
+  final String? confirmPwdError;
+  final void Function(String type, String value) handleTextFieldChange;
 
-  Widget _buildTextFormField(String label, String hintText) {
+  Widget _buildTextFormField(String type, String label, String hintText, String? errorText, TextEditingController controller, void Function(String) onChanged) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -58,7 +166,8 @@ class MobileDisplay extends StatelessWidget {
         ),
         const SizedBox(height: 5),
         TextFormField(
-          controller: TextEditingController(),
+          controller: controller,
+          onChanged: onChanged,
           decoration: InputDecoration(
             filled: true,
             fillColor: Colors.white,
@@ -68,45 +177,48 @@ class MobileDisplay extends StatelessWidget {
             ),
             hintText: hintText,
             hintStyle: const TextStyle(color: Colors.grey),
+            errorText: errorText,
+            errorMaxLines: 4
           ),
           style: const TextStyle(fontSize: 14),
-          validator: (value) {},
         ),
       ],
     );
   }
 
-  String? validateEmail(String value) {
-    RegExp regex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
-    if (value.isNotEmpty) {
-      if (!regex.hasMatch(value)) {
-        return 'Veuillez entrer une adresse e-mail valide';
-      }
-    } else {
-      return 'Veuillez entrer une adresse e-mail';
-    }
-  }
-
   Widget _buildInscriptionForm() {
     return Form(
-      key: formKey,
-      child: SizedBlock(
-        height: height * 7.3,
+      key: formKeyInscription,
+      child: Container(
+        height: height * 7.7,
         width: width * 1.1,
-        percentRadius: 0.03,
-        clickEvent: () {  },
+        decoration: BoxDecoration(
+          color: darkGlass,
+          borderRadius: BorderRadius.circular(height * 7.3 * 0.03),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 2,
+              offset: const Offset(2, 2),
+            ),
+          ],
+        ),
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              _buildTextFormField("Nom :", "Entrez votre nom"),
-              _buildTextFormField("Prénom :", "Entrez votre prénom"),
-              _buildTextFormField("E-mail :", "Entrez votre e-mail"),
-              _buildTextFormField("Nom d'utilisateur :", "Entrez votre nom d'utilisateur"),
-              _buildTextFormField("Mot de passe :", "Choisissez un mot de passe"),
+              _buildTextFormField("nom", "Nom :", "Entrez votre nom", nameError, nameController, (value) => handleTextFieldChange("nom", value)),
+              _buildTextFormField("prenom", "Prénom :", "Entrez votre prénom", nameError, firstnameController, (value) => handleTextFieldChange("prenom", value)),
+              _buildTextFormField("email", "E-mail :", "Entrez votre e-mail", emailError, emailController, (value) => handleTextFieldChange("email", value)),
+              _buildTextFormField("pseudo", "Nom d'utilisateur :", "Choisissez votre nom d'utilisateur", usernameError, usernameController, (value) => handleTextFieldChange("pseudo", value)),
+              _buildTextFormField("mdp", "Mot de passe :", "Choisissez un mot de passe", pwdError, pwdController, (value) => handleTextFieldChange("mdp", value)),
               TextFormField(
-                controller: TextEditingController(),
+                controller: confirmPwdController,
+                onChanged: (value) {
+                  handleTextFieldChange("confirmMdp", value);
+                },
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.white,
@@ -116,9 +228,10 @@ class MobileDisplay extends StatelessWidget {
                   ),
                   hintText: "Confirmez votre mot de passe",
                   hintStyle: const TextStyle(color: Colors.grey),
+                  errorText: confirmPwdError,
+                  errorMaxLines: 4
                 ),
-                style: const TextStyle(fontSize: 14),
-                validator: (value) {},
+                style: const TextStyle(fontSize: 14)
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 10.0),
@@ -139,7 +252,7 @@ class MobileDisplay extends StatelessWidget {
                   ),
                   child: ElevatedButton(
                       onPressed: () {
-                        if (formKey.currentState!.validate()) {
+                        if (formKeyInscription.currentState!.validate()) {
                         }
                       },
                       style: ElevatedButton.styleFrom(
@@ -147,7 +260,7 @@ class MobileDisplay extends StatelessWidget {
                         foregroundColor: Colors.white,
                         backgroundColor: Colors.transparent,
                       ),
-                      child: const Text("Je m\'inscris")
+                      child: const Text("Je m'inscris")
                   ),
                 ),
               ),
@@ -160,19 +273,30 @@ class MobileDisplay extends StatelessWidget {
 
   Widget _buildConnexionForm() {
     return Form(
-      key: formKey,
-      child: SizedBlock(
-        height: height * 4,
+      key: formKeyConnexion,
+      child: Container(
+        height: height * 3.4,
         width: width * 1.1,
-        percentRadius: 0.03,
-        clickEvent: () {  },
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: darkGlass,
+          borderRadius: BorderRadius.circular(height * 4 * 0.055),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 2,
+              offset: const Offset(2, 2),
+            ),
+          ],
+        ),
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              _buildTextFormField("Nom d'utilisateur :", "Entrez votre nom d'utilisateur"),
-              _buildTextFormField("Mot de passe :", "Entrez votre mot de passe"),
+              _buildTextFormField("pseudo", "Nom d'utilisateur :", "Entrez votre nom d'utilisateur", usernameError, usernameController, (value) => handleTextFieldChange("pseudo", value)),
+              _buildTextFormField("mdp", "Mot de passe :", "Entrez votre mot de passe", pwdError, pwdController, (value) => handleTextFieldChange("mdp", value)),
               Padding(
                 padding: const EdgeInsets.only(top: 10.0),
                 child: Container(
@@ -192,7 +316,7 @@ class MobileDisplay extends StatelessWidget {
                   ),
                   child: ElevatedButton(
                       onPressed: () {
-                        if (formKey.currentState!.validate()) {
+                        if (formKeyConnexion.currentState!.validate()) {
                         }
                       },
                       style: ElevatedButton.styleFrom(
