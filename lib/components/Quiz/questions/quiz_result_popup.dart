886aef2c33999b3088e0856b1d394da06923fedd
KillianFvt <killian.fauvette@gmail.com>
2024-06-06 21:20:17 +0200

    finished result popup styling

--- lib/components/Quiz/questions/quiz_result_popup.dart
+++ lib/components/Quiz/questions/quiz_result_popup.dart
@@ -1,7 +1,8 @@
+import 'package:blurrycontainer/blurrycontainer.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui' show FontWeight, ImageFilter;
-
-import 'package:my_quiz_ap/helpers/Colors.dart';
+import 'package:my_quiz_ap/helpers/Colors.dart' show electricBlue;
+import 'package:my_quiz_ap/helpers/utils.dart' show printInfo;
 
 class QuizResultPopup extends StatefulWidget {
   const QuizResultPopup({
@@ -27,84 +28,74 @@ class _QuizResultPopupState extends State<QuizResultPopup> {
 
   @override
   Widget build(BuildContext context) {
+    printInfo("Note: $_note");
     return PopScope(
         canPop: true,
         child: Center(
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.75),
-              borderRadius: BorderRadius.circular(30),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.3),
-                  blurRadius: 15,
-                  spreadRadius: -7.5,
-                  offset: const Offset(0, 15),
-                ),
-              ],
-            ),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.75,
-              height: MediaQuery.of(context).size.height * 0.25,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                  child: Padding(
-                    padding: const EdgeInsets.all(24.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
+          child: BlurryContainer(
+            blur: 30,
+            color: Colors.black.withOpacity(0.4),
+            elevation: 15,
+            shadowColor: Colors.black,
+            borderRadius: BorderRadius.circular(20),
+            width: MediaQuery.of(context).size.width * 0.75,
+            height: MediaQuery.of(context).size.height * 0.25,
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
 
-                        Column(
-                          children: [
-                            const Text(
-                              "Vous avez obtenu",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontFamily: "Quicksand",
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-
-                            const SizedBox(height: 10),
-
-                            Text(
-                              "$_note/20",
-                              style: const TextStyle(
-                                color: Colors.black,
-                                fontSize: 32,
-                                fontFamily: "Quicksand",
-                                fontWeight: FontWeight.w600,
-                              ),
-                            )
-                          ],
+                  Column(
+                    children: [
+                      const Text(
+                        "Vous avez obtenu",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: "Quicksand",
+                          fontWeight: FontWeight.w600,
                         ),
+                      ),
 
-                        const Spacer(),
+                      const SizedBox(height: 10),
 
-                        MaterialButton(
-                          onPressed: sendScore,
-                          color: electricBlue,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: const Text(
-                            "Terminer le quiz",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontFamily: "Quicksand",
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
+                      Text(
+                        "$_note/20",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontFamily: "Quicksand",
+                          fontWeight: FontWeight.w600,
                         ),
-                      ],
+                      )
+                    ],
+                  ),
+
+                  const Spacer(),
+
+                  MaterialButton(
+                    onPressed: () {
+                      sendScore;
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pop();
+                    },
+                    color: electricBlue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: const Text(
+                      "Terminer le quiz",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontFamily: "Quicksand",
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
             ),
           )
@@ -121,10 +112,23 @@ void displayResultPopup(
   showDialog(
     context: context,
     barrierDismissible: false,
-    barrierColor: const Color(0x60000000),
-    builder: (context) => QuizResultPopup(
-      score: score,
-      total: total,
+    barrierColor: const Color(0x00000000),
+    builder: (context) => Stack(
+      children: [
+
+        BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+            )
+        ),
+
+        QuizResultPopup(
+          score: score,
+          total: total,
+        ),
+      ],
     ),
   );
 }
