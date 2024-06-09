3b9f196fcde3b0209d468e04a6c76ea7d913cb32
KillianFvt <killian.fauvette@gmail.com>
2024-06-09 16:47:51 +0200

    fixed height problem in responsive web in result popup 2

--- lib/components/Quiz/questions/quiz_result_popup.dart
+++ lib/components/Quiz/questions/quiz_result_popup.dart
@@ -61,7 +61,7 @@ class _QuizResultPopupState extends State<QuizResultPopup> {
             shadowColor: electricBlue,
             borderRadius: BorderRadius.circular(20),
             width: (MediaQuery.of(context).size.width * 0.75).clamp(0, 500),
-            height: 200,
+            height: 220,
             child: Padding(
               padding: const EdgeInsets.all(24.0),
               child: Column(
