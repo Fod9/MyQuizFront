447e3a3e028c41e5e8317fa2c003eb5bfb71291b
Sandra <sandra.sini@ecoles-epsi.net>
2024-08-31 16:04:19 +0200

    homepage mobile & desktop

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -17,23 +17,133 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     String screenType = getScreenType(context);
 
-    if (screenType == "mobile") {
+    if (screenType == "desktop") {
+      _width = 410;
+      _height = 140;
+      return const DesktopDisplay();
+    } else {
       _width = MediaQuery.of(context).size.width * 0.8;
       _height = MediaQuery.of(context).size.height * 0.1;
+      return MobileDisplay(
+          width: _width, height: _height, verticalPosition: _verticalPosition
+      );
     }
+  }
+}
 
-    return MobileDisplay(
-        width: _width, height: _height, verticalPosition: _verticalPosition);
+class ContentRow extends StatelessWidget {
+  final String text;
+  final String description;
+  final String imagePath;
+  final bool isImageFirst;
+
+  const ContentRow({
+    super.key,
+    required this.text,
+    required this.description,
+    required this.imagePath,
+    this.isImageFirst = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const double paddingValue = 12.0;
+
+    Widget textColumn = Expanded(
+      flex: 2,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              text,
+              textAlign: TextAlign.justify,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: paddingValue, horizontal: paddingValue * 5),
+            child: Text(
+              description,
+              textAlign: TextAlign.justify,
+              style: const TextStyle(
+                fontSize: 22,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    Widget imageColumn = Expanded(
+      flex: 2,
+      child: Image.asset(
+        imagePath,
+        fit: BoxFit.cover,
+        scale: 0.5,
+      ),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+      child: Row(
+        children: isImageFirst
+            ? [imageColumn, textColumn]
+            : [textColumn, imageColumn],
+      ),
+    );
+  }
+}
+
+class DesktopDisplay extends StatelessWidget {
+  const DesktopDisplay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 18.0),
+        child: Column(
+          children: [
+            ContentRow(
+              text: 'Découvrez MyQuiz : votre compagnon d\'apprentissage',
+              description:
+              'Bienvenue sur MyQuiz, l\'application conçue pour transformer votre expérience d\'apprentissage. Destinée aux étudiants du supérieur, MyQuiz vous offre un accès direct à des quiz personnalisés qui vous aident à renforcer vos connaissances et à mieux vous préparer pour vos examens. Grâce à des contenus spécialement élaborés par vos enseignants, vous pouvez réviser de manière ciblée et interactive.',
+              imagePath: 'assets/images/image1.png',
+              isImageFirst: false,
+            ),
+            ContentRow(
+              text: 'Des quiz personnalisés créés par vos enseignants',
+              description:
+              "Avec MyQuiz, les enseignants ont la possibilité de créer des quiz de manière simple et rapide. Ils peuvent soit générer des questions manuellement, soit utiliser notre intelligence artificielle pour créer des quiz à partir de documents PDF de leurs cours. Cette technologie permet de transformer le contenu des cours en questions pertinentes et adaptées au niveau de chaque étudiant, garantissant ainsi une expérience d'apprentissage optimisée.",
+              imagePath: 'assets/images/image2.png',
+              isImageFirst: true,
+            ),
+            ContentRow(
+              text: 'Améliorez vos résultats en toute simplicité',
+              description:
+              "MyQuiz est plus qu'un simple outil d'évaluation ; c'est un véritable atout pour maximiser vos performances académiques. En vous offrant des quiz accessibles à tout moment et sur n'importe quel appareil, MyQuiz vous permet de vous entraîner régulièrement et de mesurer vos progrès. Que vous soyez en déplacement ou à la maison, votre révision est à portée de main, vous aidant à atteindre vos objectifs académiques avec succès.",
+              imagePath: 'assets/images/image3.png',
+              isImageFirst: false,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
 class MobileDisplay extends StatelessWidget {
   MobileDisplay(
-      {Key? key,
+      {super.key,
         required this.width,
         required this.height,
-        required this.verticalPosition})
-      : super(key: key);
+        required this.verticalPosition});
 
   final double width;
   final double height;
